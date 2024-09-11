efe9fd102167ed0e139b861d9eccb6f5a336dffe
unknown <altamash rashid>
2024-09-11 10:00:34 +0530

    user role added

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\DoctorController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
@@ -11,5 +13,17 @@ Route::view('dashboard', 'dashboard')
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
+
+
+
+Route::get('/admin/dashboard',[AdminController::class, 'loadAdminDashboard'])
+    ->name('admin-dashbaord')
+    ->middleware('Admin');
+
+
+Route::get('/doctor/dashboard',[DoctorController::class, 'loadDoctorDashboard'])
+    ->name('doctor-dashbaord')
+    ->middleware('Doctor');
+
 
 require __DIR__.'/auth.php';
